b365fffd991ef5e37e10c2930faf5033086a9fbf
TheMOON <mert__balkan@hotmail.com>
2022-03-02 21:41:43 +0300

    Character move script

--- DesignPatterns/Assets/GeneralScripts/CharacterMove.cs
+++ DesignPatterns/Assets/GeneralScripts/CharacterMove.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float moveSpeed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalMove, 0, verticalMove) * Time.deltaTime * moveSpeed);
     }
 }
